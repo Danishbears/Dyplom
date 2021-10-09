50473e1029eb43dc3798c743b53a088a1adec9dd
Admin <danishbears@gmail.com>
2021-10-10 00:37:54 +0300

    Added click listener to Login button. Added a function to check password and email

--- app/src/main/java/com/example/diplomich/Authentication/LoginActivity.kt
+++ app/src/main/java/com/example/diplomich/Authentication/LoginActivity.kt
@@ -2,11 +2,15 @@ package com.example.diplomich.Authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
 import com.example.diplomich.R
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
@@ -27,5 +31,37 @@ class LoginActivity : AppCompatActivity() {
         loginToRegText = findViewById(R.id.RegisterText)
         progressBar = findViewById(R.id.progressBarLogin)
 
+        loginButton.setOnClickListener{
+            performLogin()
+        }
+
+    }
+    private fun performLogin(){
+        var email:String = loginMail.text.toString().trim()
+        var password:String = loginPassword.text.toString().trim()
+        if(TextUtils.isEmpty(email)){
+            loginMail.error = "Email is Required"
+            return
+        }
+        if(TextUtils.isEmpty(password)){
+            loginPassword.error = "Password is Required"
+            return
+        }
+        if(password.length <6){
+            loginPassword.error ="Minimum length for password is 6 "
+            return
+        }
+//to check a special characters !!
+        progressBar.visibility = View.VISIBLE
+
+        //check user in database
+        fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){
+            task->onCompleteLogin(task)
+        }
+    }
+    private fun onCompleteLogin(task: Task<AuthResult>) {
+        if(task.isSuccessful){
+
+        }
     }
 }
