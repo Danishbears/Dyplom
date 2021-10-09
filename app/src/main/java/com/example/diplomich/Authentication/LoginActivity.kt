efbb6c80389b389c83e9a90250aca5bc46d1dcce
Admin <danishbears@gmail.com>
2021-10-10 00:26:14 +0300

    added fields in LoginActivity.kt

--- app/src/main/java/com/example/diplomich/Authentication/LoginActivity.kt
+++ app/src/main/java/com/example/diplomich/Authentication/LoginActivity.kt
@@ -2,11 +2,24 @@ package com.example.diplomich.Authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.example.diplomich.R
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var loginMail:EditText
+    private lateinit var loginPassword:EditText
+    private lateinit var loginButton:Button
+    private lateinit var fAuth:FirebaseAuth
+    private lateinit var loginToRegText:TextView
+    private lateinit var progressBar:ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 }
