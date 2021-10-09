d35a47578d4c1bec04ff3d9b2777ff9d4ff2e6ff
Admin <danishbears@gmail.com>
2021-10-09 16:45:04 +0300

    Defined properties

--- app/src/main/java/com/example/diplomich/Authentication/Register.kt
+++ app/src/main/java/com/example/diplomich/Authentication/Register.kt
@@ -2,11 +2,26 @@ package com.example.diplomich.Authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.example.diplomich.R
 
 class Register : AppCompatActivity() {
+    private lateinit var editTextName:EditText
+    private lateinit var editTextSurname:EditText
+    private lateinit var editTextEmail:EditText
+    private lateinit var editTextPhoneNumber:EditText
+    private lateinit var editTextPassword:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        editTextName = findViewById(R.id.editName)
+        editTextSurname = findViewById(R.id.editLastName)
+        editTextEmail = findViewById(R.id.editEmailAddress)
+        editTextPhoneNumber = findViewById(R.id.editTextPhone)
+        editTextPassword = findViewById(R.id.editPasswordAddress)
+
+
     }
 }
