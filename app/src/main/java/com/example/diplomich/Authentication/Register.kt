020cf4fe9ea7a0ad8c97f2fb5c4883c3332473c4
Admin <danishbears@gmail.com>
2021-10-09 16:47:34 +0300

    Added FireBase and ProgressBar fields

--- app/src/main/java/com/example/diplomich/Authentication/Register.kt
+++ app/src/main/java/com/example/diplomich/Authentication/Register.kt
@@ -3,7 +3,9 @@ package com.example.diplomich.Authentication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.ProgressBar
 import com.example.diplomich.R
+import com.google.firebase.auth.FirebaseAuth
 
 class Register : AppCompatActivity() {
     private lateinit var editTextName:EditText
@@ -11,6 +13,8 @@ class Register : AppCompatActivity() {
     private lateinit var editTextEmail:EditText
     private lateinit var editTextPhoneNumber:EditText
     private lateinit var editTextPassword:EditText
+    private lateinit var progressBar:ProgressBar
+    private lateinit var fAuth:FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +25,8 @@ class Register : AppCompatActivity() {
         editTextEmail = findViewById(R.id.editEmailAddress)
         editTextPhoneNumber = findViewById(R.id.editTextPhone)
         editTextPassword = findViewById(R.id.editPasswordAddress)
-
+        progressBar = findViewById(R.id.progressBar)
+        fAuth = FirebaseAuth.getInstance()
 
     }
 }
