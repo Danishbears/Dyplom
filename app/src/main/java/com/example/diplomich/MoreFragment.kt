999d19f2b764195b745af809f4e3461fe2c173d8
Admin <danishbears@gmail.com>
2021-10-10 22:23:34 +0300

    Login users can see their info

--- app/src/main/java/com/example/diplomich/MoreFragment.kt
+++ app/src/main/java/com/example/diplomich/MoreFragment.kt
@@ -5,8 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MoreFragment : Fragment() {
+    private lateinit var userEmail: TextView
+    private lateinit var userPhone: TextView
+    private lateinit var userName:TextView
+    private lateinit var userPhoto:ImageView
+    private lateinit var fAuth:FirebaseAuth
+    private lateinit var fStore:FirebaseFirestore
+    private lateinit var userId:String
 
 
     override fun onCreateView(
@@ -14,7 +28,26 @@ class MoreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false)
+        val rootView: View = inflater.inflate(R.layout.fragment_more, container, false)
+        userEmail = rootView.findViewById(R.id.userEmailInfo) as TextView
+        userPhone = rootView.findViewById(R.id.userPhoneNumber)
+        userName = rootView.findViewById(R.id.HelloUserName)
+        userPhoto = rootView.findViewById(R.id.imageUser)
+
+
+        fAuth = FirebaseAuth.getInstance()
+        fStore = FirebaseFirestore.getInstance()
+        userId = fAuth.currentUser!!.uid
+
+        val documentReference:DocumentReference = fStore.collection("users").document(userId)
+        documentReference.addSnapshotListener{snapshot, e->
+            userPhone.text = snapshot!!.getString("PhoneNumber")
+            userName.text = "${resources.getString(R.string.HelloToUser)} ${snapshot!!.getString("Name")}"
+            userEmail.text = snapshot!!.getString("Email")
+        }
+
+        return rootView
     }
+
 
 }
