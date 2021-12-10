28af85bdc03bd351e7631375cf386d941ad91de8
Danishbears <danishbears@gmail.com>
2021-12-11 01:04:24 +0200

    Fixed problem with images and Firebase

--- app/src/main/java/com/example/diplomich/adapter/MyAdapter.kt
+++ app/src/main/java/com/example/diplomich/adapter/MyAdapter.kt
@@ -1,53 +1,58 @@
 package com.example.diplomich.adapter
 
-import android.content.ClipDescription
 import android.content.Context
 import android.content.Intent
-import android.icu.number.NumberFormatter.with
-import android.icu.number.NumberRangeFormatter.with
-import android.os.Bundle
+import android.util.ArrayMap
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
-import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.GenericTransitionOptions.with
 import com.bumptech.glide.Glide
-import com.bumptech.glide.Glide.with
-import com.example.diplomich.HomeFragment
-import com.example.diplomich.MainActivity
+import com.bumptech.glide.GlideExperiments
 import com.example.diplomich.ProductDetailsActivity
 import com.example.diplomich.R
 import com.example.diplomich.ViewModel.Products
 import com.example.diplomich.`interface`.ClickInteface
-import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.UploadTask
-import com.google.firebase.storage.ktx.storage
-import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.popular_item.view.*
+
+
 class MyAdapter(var context:Context,
                 var list:List<Products>): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
     lateinit var onItemListener:ClickInteface
+    var items = arrayListOf<String>()
+    //var itemsName = mutableListOf<String,String>()
+   // var itemsName = arrayMapOf(Pair(String,String))
+    var itemsName = ArrayMap<String,String>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
         val view:View = LayoutInflater.from(context).inflate(R.layout.popular_item,parent,false)
         return MyViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: MyAdapter.MyViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val products:Products = list[position]
         holder.productName.text = products.name
         holder.productPrice.text = products.price
         holder.description.text = products.description
-        Glide.with(context)
-            .load("https://firebasestorage.googleapis.com/v0/b/dyplom-867af.appspot.com/o/Product%20Images%2Fimage%3A24Oct%2029%2C%20202114%3A11%3A02.jpg?alt=media&token=ed745a9f-2a68-47fe-a217-27efb6328609")
-            .into(holder.imageView)
+
+        holder.db = FirebaseStorage.getInstance().getReference("Product Images/")
+        holder.db.child("${products.pid}.jpg").downloadUrl.addOnCompleteListener{task->
+            products.image = task.result.toString()
+            Glide.with(context)
+                .load(products.image)
+                .into(holder.imageView.image_product)
+        }
+            .addOnFailureListener{
+                Log.d("FailedCompleteListener",it.message.toString())
+            }
+
+
         holder.imageView.setOnClickListener {
             val intent = Intent(holder.itemView.context,ProductDetailsActivity::class.java)
             intent.putExtra("pid",products.pid)
@@ -62,12 +67,21 @@ class MyAdapter(var context:Context,
         return list.size
     }
 
+    private fun checkTableItems(imageItem: String, name: String, pid: String?){
+       // items.add(imageItem)
+        var sub = ".jpg"
+        //itemsName[name.substring(0,name.indexOf(".jpg"))] = imageItem
+            itemsName[(name.substring(5, name.indexOf(".jpg")))] = imageItem
+        Log.d("CheckItems",(name.substring(5,name.indexOf(".jpg"))))
+
+    }
+
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val productName = itemView.findViewById(R.id.product_name) as TextView
         val productPrice = itemView.findViewById(R.id.product_price) as TextView
         val description = itemView.findViewById(R.id.product_description) as TextView
         val imageView = itemView.findViewById(R.id.image_product) as ImageView
-
+        lateinit var db:StorageReference
 
     }
 
